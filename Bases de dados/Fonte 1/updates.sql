1f58b7c1a9b5f7e49744dd58c551d2ff42f1eda5
Rui <Rui@MBP-de-Rui-2.home>
2017-01-19 21:42:53 +0000

    alterações

--- Bases de dados/Fonte 1/updates.sql
+++ Bases de dados/Fonte 1/updates.sql
@@ -6,3 +6,8 @@ INSERT INTO Cliente
 			(49020942,'adeus','Carlos',210492631,3);
 
 UPDATE `eurosuper`.`cliente` SET `PNome`='Amares' WHERE `CC`=13901961;
+SET SQL_SAFE_UPDATES = 0;
+
+use arearetencao;
+delete from qua_LfactosP1;
+select * fpos_LClienteP1rom equi_Cliente;
